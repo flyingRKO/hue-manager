00fb7939f683ea5238d2fb463f3fcd260cb5cf9b
flyingRKO <wogns970@naver.com>
2024-03-14 22:44:01 +0900

    Initial commit

---
+++ src/main/java/com/rko/huemanager/HueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rko.huemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HueManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rko/huemanager/HueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rko.huemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HueManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
